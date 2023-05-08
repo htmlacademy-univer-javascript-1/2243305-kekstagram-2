3e17e452d86af8779a2df44b8c5700263546849a
Konstantin Fadeev <kostya.fad1@gmail.com>
2023-05-08 19:25:58 +0500

    module12_task1

--- js/util.js
+++ js/util.js
@@ -33,11 +33,33 @@ const showAlert = (message) => {
   document.body.append(container);
 };
 
+function debounce(callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
 
 export {
   checkMaxLength,
   checkEscapePressed,
   isCheckModelOpen,
   showAlert,
+  debounce,
+  throttle,
   ErrorMessage,
 };
