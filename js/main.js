b52eae018bbd440b167612d0aca9f2e43dbe6cfd
Konstantin Fadeev <kostya.fad1@gmail.com>
2023-05-10 17:55:47 +0500

    fix bag

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-import {renderThumbnailsDefault, setDefaultPictures, setRandomPictures, setDiscussPictures} from './renderThumbnailsDefault.js';
+import {renderThumbnails, setDefaultPictures, setRandomPictures, setDiscussPictures} from './renderThumbnails.js';
 import {getData} from './api.js';
 import {setUserFormSubmit} from './userForm.js';
 import {closeBigPicture} from './renderBigPicture.js';
 
 getData((descriptions) => {
-  renderThumbnailsDefault(descriptions);
+  renderThumbnails(descriptions);
   setDefaultPictures(descriptions);
   setRandomPictures(descriptions);
   setDiscussPictures(descriptions);
